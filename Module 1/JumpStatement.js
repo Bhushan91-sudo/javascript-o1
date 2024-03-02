e9f75182094bf2b2f9a9e1b379dde3db8d64750c
Bhushan Patle <104772925+Bhushan91-sudo@users.noreply.github.com>
2024-03-02 08:27:59 +0000

    local

---
+++ Module 1/JumpStatement.js
@@ -0,0 +1,15 @@
+// Input num valus is in variable *num*
+var num = parseInt(readLine().trim());
+function printOddNumbers(N) {
+    for (let i = 0; i < N; i++) {
+        if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+}
+
+//Sample Input
+
+
+// Function Call
+printOddNumbers(num);
