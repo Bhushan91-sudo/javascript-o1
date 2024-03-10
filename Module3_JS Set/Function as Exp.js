0f32a571ddc8f67f122b7750efd2d796654e6bb6
Bhushan Patle <104772925+Bhushan91-sudo@users.noreply.github.com>
2024-03-10 17:55:33 +0000

    local

---
+++ Module3_JS Set/Function as Exp.js
@@ -0,0 +1,9 @@
+var add = function(a ,b) {
+    return a+b
+}
+
+var sum = add
+
+var result = sum(3 ,4)
+
+console.log(result)
---
+++ Module3_JS Set/Hoisting.js
@@ -0,0 +1,14 @@
+console.log(a)
+greet()
+
+add(2 ,4)
+
+var a = 'hi'
+
+function greet() {
+    console.log('Hello bhushan')
+}
+
+var add = function(a ,b) {
+    console.log(a+b)
+}
