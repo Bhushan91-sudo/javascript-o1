d36ae51c959c2e85fced60b88c027fc94cc2a750
Bhushan Patle <104772925+Bhushan91-sudo@users.noreply.github.com>
2024-03-10 17:06:50 +0000

    local

---
+++ Module3_JS Set/object.js
@@ -0,0 +1,19 @@
+/**
+ * In Js object are in key value pair
+ */
+
+var person = {
+    firstName : 'Adam',
+   
+    secondName : 'Sumit',
+
+    age : 25,
+
+    ownsCar : false
+}
+
+//console.log(person)
+
+//dot natation 
+
+console.log(person.age)
