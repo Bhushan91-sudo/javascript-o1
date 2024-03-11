bd6cdb857351fbe025e78982945de29196ddf38c
Bhushan Patle <104772925+Bhushan91-sudo@users.noreply.github.com>
2024-03-11 06:10:53 +0000

    arrow

---
+++ Module3_JS Set/Arrow.js
@@ -0,0 +1,17 @@
+let test1 = function() {
+    console.log(1)
+}
+
+let test2 = function(a) {
+    console.log(a+2)
+}
+
+let test3 = function(a, b) {
+    console.log(a+b)
+}
+
+test1()
+
+test2(4)
+
+test3(2 ,6)
